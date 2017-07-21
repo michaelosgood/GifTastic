abd480966745437f2799f613de020eaa86583d77
Michael Osgood <michaelosgood@dhcp-v241-010.pv.reshsg.uci.edu>
2017-07-20 22:39:37 -0700

    making progress

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 console.log("html is connected to app.js")
 
-
-$("button").on("click", function() { //click event listener for all buttons
+//click event listener for all buttons
+$("button").on("click", function() { 
 	//button clicked
 	console.log("you clicked me");
 
 	//grabbing and storing data-hero property from button
-	var hero = $(this).attr("data-hero"); 
+	var hero = $(this).attr("data-character"); 
 
 	//our queryURL has a hero's name concatenated in
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+hero+"&api_key=0c9a3a752e1346f7b0c2bc714127e701&limit=10";
@@ -21,5 +21,35 @@ $("button").on("click", function() { //click event listener for all buttons
 	.done(function(response) {
 		console.log(queryURL);
 		console.log(response);
+
+		//stores the data from the request in the results variable
+		var results = response.data;
+
+		//we loop through each result
+		for(var i = 0; i < results.length; i++){
+
+			//creating and storing a div
+			var characterDiv = $("<div>");
+			console.log("characterDiv created");
+
+			//creating a paragraph div tag with the results' ratings
+			var p = $("<p>").text("Rating: " + results[i].rating);
+
+			//creating and storing an image tag
+			var characterImage = $("<img>")
+
+			//setting the src attribute to a property pulled from the results
+			characterImage.attr("src", results[i].images.fixed_height.url);
+
+			//appending paragraph div to the rating
+			characterDiv.append(p);
+			characterDiv.append(characterImage);
+
+			//prepending the characterDiv to the HTML page (my div with id "gifs-appear-here")
+			$("#gifs-appear-here").prepend(characterDiv);
+		}
 	})
 });
+
+
+
